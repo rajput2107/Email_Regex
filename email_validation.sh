1a8c758294cc1d636b3781f8b86c1c4e8c2fc837
rajput2107 <pramodrana2107@gmail.com>
2020-07-15 10:37:50 +0530

    Passowrd Min Length 8

--- email_validation.sh
+++ email_validation.sh
@@ -5,13 +5,14 @@ read -p "Enter First name: " Fname
 read -p "Enter Last name: " Lname
 read -p "Enter Email: " email
 read -p "Enter Mobile:" mobile
+read -p "Enter Password: " password
 
 
 Fname_Pattern="^[A-Z][a-z]{2,}$"
 Lname_Pattern="^[A-Z][a-z]{2,}$"
 email_Pattern="^[A-Za-z0-9]+([_+-.][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})?$"
 mobile_Pattern="^[+][1-9]{1,3}[ ]{1}[1-9][0-9]{9}$"
-
+pass_Pattern="^.{8,}"
 
 if [[ $Fname =~ $Fname_Pattern ]] && [[ $Lname =~ $Lname_Pattern ]]
 then
@@ -34,3 +35,12 @@ then
 else
         echo "Invalid Mobile"
 fi
+
+
+if [[ $password =~ $pass_Pattern ]]
+then
+        echo "Valid Password"
+else
+        echo "Invalid Password. Min length should be 8"
+fi
+
